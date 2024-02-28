b2336431155f19f365c1852aa8f0dc8fb925c5a1
xWALL-ed <walid.alhasan530@gmail.com>
2024-02-28 18:01:10 +0600

    Disabled overflow when nav oppened

--- 3-Rotating_Navigation/script.js
+++ 3-Rotating_Navigation/script.js
@@ -3,14 +3,18 @@ var btnClose = document.getElementById("close");
 var circle = document.getElementById("circle");
 var container = document.querySelector(".container");
 var nav = document.querySelector(".nav-container");
+var body = document.querySelector("body");
 
 btnOpen.addEventListener('click',()=>{
     circle.classList.add("active");
     container.classList.add("show-nav");
     nav.classList.add("active");
+    body.style.overflow="hidden";
 });
 btnClose.addEventListener('click',()=>{
     circle.classList.remove("active");
     container.classList.remove("show-nav");
     nav.classList.remove("active");
+    body.style.overflow="auto";
+    body.style.overflowX="hidden";
 });
